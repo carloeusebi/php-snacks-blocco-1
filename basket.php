45485aab0de328ca63bc1b00d4500ad8f78b7b92
carloeusebi <carloeusebi@gmail.com>
2023-07-17 13:35:19 +0200

    first commit

---
+++ basket.php
@@ -0,0 +1,37 @@
+<?php
+
+$matches = [
+  [
+    'home' => [
+      'name' => 'VL Pesaro',
+      'points' => 85
+    ],
+    'guest' => [
+
+      'name' => 'Brescia',
+      'points' => 76
+    ]
+  ],
+  [
+    'home' => [
+      'name' => 'Olimpia Milano',
+      'points' => 55
+    ],
+    'guest' => [
+
+      'name' => 'Virtus Bologna',
+      'points' => 60
+    ]
+  ],
+  [
+    'home' => [
+      'name' => 'Trieste',
+      'points' => 45
+    ],
+    'guest' => [
+
+      'name' => 'Dinamo Sassari',
+      'points' => 89
+    ]
+  ],
+];

56aeb78555878d03110086e47c41e4d6e277c4d6
carloeusebi <carloeusebi@gmail.com>
2023-07-17 13:46:26 +0200

    Added html for validation form | snack 2

--- index.php
+++ index.php
@@ -3,14 +3,34 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta age="viewport" content="width=device-width, initial-scale=1.0">
   <title>php-snacks-blocco-1</title>
 </head>
 
 <body>
   <main>
+    <!--  LINK TO BASKET MATCHES -->
     <section id="basket">
+      <h2>Basket games</h2>
       <a href="basket.php">Check basket games</a>
+      <hr>
+    </section>
+
+
+    <!-- VERIFY FORM -->
+    <section id="verify-form">
+      <h2>Verify:</h2>
+      <form action="verify.php" method="get" novalidate>
+        <!-- name -->
+        <label for="name">Name: </label>
+        <input type="text" name="name" id="name">
+        <!-- mail -->
+        <label for="email">Email: </label>
+        <input type="text" name="email" id="email">
+        <!-- age -->
+        <label for="age">Age: </label>
+        <input type="text" name="age" id="age">
+      </form>
     </section>
   </main>
 </body>
